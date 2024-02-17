c49f97157838221c9ef3b7f6d329f8306aaa18ce
KirillSerg <kirill.sergienko1989@gmail.com>
2024-02-18 01:09:30 +0200

    feat: added home > hero section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,16 @@
-import style from "./Home.module.scss";
+import styles from "./Home.module.scss";
 
 const Home = () => {
-  return <div className={style.wrap}>Blog</div>;
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.hero}>
+        <div className={styles.title}>
+          <h1>Find your home in Italy</h1>
+          <p>Search over 40,000 properties for sale</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
